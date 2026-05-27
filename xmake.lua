755a454605e566fa40eedb4522c186e3e3d8478e
enig Him <2715576845@qq.com>
2026-05-27 09:59:00 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,9 +1,14 @@
 add_rules("mode.debug", "mode.release")
 
-add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
+add_repositories("levimc-repo https://github.com/LiteLDev/xmake-repo.git")
 
--- LeviLamina
-add_requires("levilamina 1.0.0", {configs = {target_type = "server"}})
+option("target_type")
+    set_default("server")
+    set_showmenu(true)
+    set_values("server", "client")
+option_end()
+
+add_requires("levilamina", {configs = {target_type = get_config("target_type")}})
 add_requires("levibuildscript")
 
 if not has_config("vs_runtime") then
@@ -13,41 +18,18 @@ end
 target("StarrySkyForm")
     add_rules("@levibuildscript/linkrule")
     add_rules("@levibuildscript/modpacker")
-    add_rules("plugin.compile_commands.autoupdate")
+    add_cxflags("/EHa", "/utf-8", "/W4", "/w44265", "/w44289", "/w44296", "/w45263", "/w44738", "/w45204")
+    add_defines("NOMINMAX", "UNICODE", "LDAPI_EXPORT")
+    add_packages("levilamina")
     set_exceptions("none")
     set_kind("shared")
     set_languages("c++20")
     set_symbols("debug")
-    add_cxflags(
-        "/EHa",
-        "/utf-8",
-        "/W4",
-        "/w44265",
-        "/w44289",
-        "/w44296",
-        "/w45263",
-        "/w44738",
-        "/w45204"
-    )
-    add_defines(
-        "NOMINMAX",
-        "UNICODE",
-        "LDAPI_EXPORT"
-    )
-    add_includedirs("src")
+    add_headerfiles("src/**.h")
     add_files("src/**.cpp", "src/**.cc")
-    add_headerfiles("src/(starryskyform/**.h)")
-
-    add_packages(
-        "levilamina"
-    )
-
+    add_includedirs("src")
     add_configfiles("src/BuildInfo.h.in")
     set_configdir("src/starryskyform")
-
-    if is_mode("debug") then
-        add_defines("DEBUG")
-    end
 
     after_build(function (target)
         local bindir = path.join(os.projectdir(), "bin")
